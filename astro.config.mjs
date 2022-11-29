87fc11f5c88020fd61ae4bb5571332c856e61c65
Xeraph <61700204+adrianrl99@users.noreply.github.com>
2022-11-29 00:16:57 -0500

    base project

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import mdx from "@astrojs/mdx";
+import astroI18next from "astro-i18next";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://xeraph.dev",
+  integrations: [
+    tailwind(),
+    compress(),
+    robotsTxt(),
+    sitemap({
+      i18n: {
+        defaultLocale: "en",
+        locales: {
+          en: "en",
+          es: "es",
+        },
+      },
+    }),
+    mdx(),
+    astroI18next(),
+  ],
+});
